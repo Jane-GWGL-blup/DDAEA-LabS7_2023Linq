fe8828b248737b850e66298d8783ca8fe879d991
nicole <nicole@gmail.com>
2023-05-08 21:08:11 -0500

    Se creo los metodos estaticos

--- Linq2023/Program.cs
+++ Linq2023/Program.cs
@@ -9,36 +9,111 @@ namespace Linq2023
 {
     public class Program
     {
+        public static DataClasses1DataContext context = new DataClasses1DataContext();
+
         static List<Product> products = new List<Product>();
         static void Main(string[] args)
         {
-            //int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
-            //ShowPares(numbers);
-            //ShowParesLambda(numbers);
+            /*IntroToLINQ();
+            DataSource();
+            Filtering();
+            Ordering();
+            Grouping();
+            Grouping2();
+            Joining();*/
 
-            InsertProducts();
-            var productsExpensive = products.Where(x => x.Price > 50).ToList();
-            //<50 y que contega la letra m
-            var productsCheap = products.Where(x=> x.Price < 50 && x.Name.Contains("m")).ToList();
-            //Objeto dinamico
-            var productsNew = products.Where(x => x.Price < 50 && x.Name.Contains("m"))
-                                        .Select(x => new
-                                        {
-                                            Nombre = x.Name,
-                                            PrecioIGV= x.Price*1.18
-                                        }).ToList();
-
-            foreach (var item in productsNew)
-            {
-                Console.Write(item.Nombre);
-                Console.WriteLine(item.PrecioIGV);
-                Console.WriteLine();
-
-            }
-
-            Console.ReadLine();
+            Console.Read();
 
         }
+        static void IntroToLINQ()
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery = 
+                from num in numbers where (num % 2) == 0 select num;
+
+            foreach(int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+        }
+        
+        static void DataSource()
+        {
+            var queryAllCustomers = from cust in context.clientes select cust;
+
+            foreach(var item in queryAllCustomers)
+            {
+                Console.WriteLine(item.NombreCompañia);
+            }
+        }
+        
+        static void Filtering()
+        {
+            var queryLondonCustomers = from cust in context.clientes
+                                       where cust.Ciudad == "Londres"
+                                       select cust;
+
+            foreach(var item in queryLondonCustomers)
+            {
+                Console.WriteLine(item.Ciudad);
+            }
+        }
+
+        static void Ordering()
+        {
+            var queryLondonCustomers = from cust in context.clientes
+                                       where cust.Ciudad == "London"
+                                       orderby cust.NombreCompañia ascending
+                                       select cust;
+
+            foreach(var item in queryLondonCustomers)
+            {
+                Console.WriteLine(item.NombreCompañia);
+            }
+        }
+        
+        static void Grouping()
+        {
+            var queryCustomersByCity = from cust in context.clientes
+                                       group cust by cust.Ciudad;
+
+            foreach(var customerGroup in queryCustomersByCity)
+            {
+                Console.WriteLine(customerGroup.Key);
+                foreach(clientes customer in customerGroup)
+                {
+                    Console.WriteLine("      {0}", customer.NombreCompañia);
+                }
+            }
+        }
+
+        static void Grouping2()
+        {
+            var custQuery = from cust in context.clientes
+                            group cust by cust.Ciudad into custGroup
+                            where custGroup.Count() > 2
+                            orderby custGroup.Key
+                            select custGroup;
+            
+            foreach(var item in custQuery)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        static void Joining()
+        {
+            var innerJoinQuery = from cust in context.clientes
+                                 join dist in context.Pedidos on cust.idCliente equals dist.IdCliente
+                                 select new { CustomerName = cust.NombreCompañia, DistributorName = dist.PaisDestinatario };
+
+            foreach(var item in innerJoinQuery)
+            {
+                Console.WriteLine(item.CustomerName);
+            }
+        }
+
 
         private static void ShowPares(int[] numbers)
         {
